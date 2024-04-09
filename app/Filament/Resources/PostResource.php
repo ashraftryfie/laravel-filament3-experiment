80d6982358f81aa949be76cea1cded946bd3ef46
Ashraf <ashraf.tryfie.dev@gmail.com>
2024-04-09 19:47:47 +0300

    Add: Tabs for post resource form.

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -4,19 +4,20 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource\RelationManagers;
-use App\Models\Category;
 use App\Models\Post;
 use App\PostStatus;
 use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\Group;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\IconPosition;
 use Filament\Tables;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\ImageColumn;
@@ -35,58 +36,66 @@ class PostResource extends Resource
     {
         return $form
             ->schema([
-                Section::make('Create a New Post')
-                    ->description('create a new post using this form')
-                    //->collapsed()
-                    ->schema([
-                        TextInput::make('title')
-                            ->rules([
-                                'min:3',
-                                'max:25',
-                            ])
-                            ->required(),
 
-                        TextInput::make('slug')
-                            ->minLength(3)
-                            ->maxLength(30)
-                            ->unique(ignoreRecord: true)
-                            ->required(),
-
-                        Select::make('status')
-                            ->options(PostStatus::class)
-                            ->required(),
-
-                        Select::make('category_id')
-                            ->required()
-                            ->label('Category')
-                            //->options(Category::all()->pluck('name', 'id'))
-                            ->relationship(
-                                'category',
-                                'name'
-                            )
-                            ->searchable(),
-
-                        MarkdownEditor::make('content')
-                            ->columnSpanFull()
-                            ->required(),
-                    ])->columnSpan(2)->columns(2),
-
-                Group::make()->schema([
-                    Section::make('Choose an Image')
-                        ->collapsible()
+                Tabs::make('Create a New Post')->tabs([
+                    Tab::make('Basic Form')
+                        ->icon('heroicon-s-inbox')
+                        ->badge('required')
                         ->schema([
-                            FileUpload::make('thumbnail')
-                                ->disk('public')
-                                ->directory('thumbnails'),
-                        ])->columnSpan(1),
+                            TextInput::make('title')
+                                ->rules([
+                                    'min:3',
+                                    'max:25',
+                                ])
+                                ->required(),
 
-                    ColorPicker::make('color')->required(),
+                            TextInput::make('slug')
+                                ->minLength(3)
+                                ->maxLength(30)
+                                ->unique(ignoreRecord: true)
+                                ->required(),
 
-                    TagsInput::make('tags')->required(),
-                ]),
+                            Select::make('status')
+                                ->options(PostStatus::class)
+                                ->required(),
+
+                            Select::make('category_id')
+                                ->required()
+                                ->label('Category')
+                                //->options(Category::all()->pluck('name', 'id'))
+                                ->relationship(
+                                    'category',
+                                    'name'
+                                )
+                                ->searchable(),
+
+                            MarkdownEditor::make('content')
+                                ->columnSpanFull()
+                                ->required(),
+                        ]),
+
+                    Tab::make('Meta Data')
+                        ->icon('heroicon-s-plus')
+                        ->iconPosition(IconPosition::After)
+                        ->schema([
+                            Section::make('Choose an Image')
+                                ->collapsible()
+                                ->schema([
+                                    FileUpload::make('thumbnail')
+                                        ->disk('public')
+                                        ->directory('thumbnails'),
+                                ])->columnSpan(1),
+
+                            ColorPicker::make('color')->required(),
+
+                            TagsInput::make('tags')->required(),
+                        ]),
+                ])->activeTab(1)
+                    ->persistTabInQueryString()
+                    ->columnSpanFull(),
 
             ])->columns([
-                'default' => 1,
+                'default' => 3,
                 'md' => 2,
                 'lg' => 3,
                 'xlg' => 4,
