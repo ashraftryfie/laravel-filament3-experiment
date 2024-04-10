607786e75dde6e0cb562f7962a7af286c3a0fde7
Ashraf <ashraf.tryfie.dev@gmail.com>
2024-04-10 21:42:11 +0300

    Add: Table Tabs for status in the post resource.

--- app/Filament/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Resources/PostResource/Pages/ListPosts.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListPosts extends ListRecords
 {
@@ -16,4 +18,20 @@ class ListPosts extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    public function getTabs(): array {
+        return [
+            'All' => Tab::make(),
+            'Pending' => Tab::make()->modifyQueryUsing(function (Builder $query) {
+                return $query->where('status', 'pending');
+            }),
+            'Published' => Tab::make()->modifyQueryUsing(function (Builder $query) {
+                return $query->where('status', 'published');
+            }),
+            'Blocked' => Tab::make()->modifyQueryUsing(function (Builder $query) {
+                return $query->where('status', 'blocked');
+            }),
+        ];
+    }
+
 }
