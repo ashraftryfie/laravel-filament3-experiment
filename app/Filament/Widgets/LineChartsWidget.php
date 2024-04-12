eda9ecc29c5f0cb7c2aa4d95a5895e1f8d07d139
Ashraf <ashraf.tryfie.dev@gmail.com>
2024-04-12 12:04:39 +0300

    Refactor: Dynamically populate line chart users data using trend package.

--- app/Filament/Widgets/LineChartsWidget.php
+++ app/Filament/Widgets/LineChartsWidget.php
@@ -2,7 +2,10 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\User;
 use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class LineChartsWidget extends ChartWidget
 {
@@ -14,16 +17,35 @@ class LineChartsWidget extends ChartWidget
 
     protected function getData(): array
     {
+//        return [
+//            'datasets' => [
+//                [
+//                    'label' => 'Blog posts created',
+//                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+//                    'backgroundColor' => '#36A2EB',
+//                    'borderColor' => '#9BD0F5',
+//                ],
+//            ],
+//            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+//        ];
+        $data = Trend::model(User::class)
+            ->between(
+                start: now()->subMonths(10),  // last 6 months
+                end: now(),
+            )
+            ->perMonth()
+            ->count();
+
+        //dd($data); // testing data
+
         return [
             'datasets' => [
                 [
-                    'label' => 'Blog posts created',
-                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
-                    'backgroundColor' => '#36A2EB',
-                    'borderColor' => '#9BD0F5',
+                    'label' => 'Newly joined users',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
                 ],
             ],
-            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
         ];
     }
 
