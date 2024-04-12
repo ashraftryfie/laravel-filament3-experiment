0b4d607f4860d29ecff4344abf08bc70c33d940a
Ashraf <ashraf.tryfie.dev@gmail.com>
2024-04-12 10:03:54 +0300

    Add: Auto Slug Generation using afterStateUpdated.

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -5,15 +5,15 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\CategoryResource\Pages;
 use App\Filament\Resources\CategoryResource\RelationManagers;
 use App\Models\Category;
-use Filament\Forms;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class CategoryResource extends Resource
 {
@@ -25,8 +25,29 @@ class CategoryResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('name')->required(),
-                TextInput::make('slug')->required(),
+                TextInput::make('name')->required()
+                    ->minLength(1)->maxLength(100)
+                    ->live(  // livewire for realtime update slug
+                        onBlur: True, // generate slug after we end focusing on name input field
+                    )
+                    ->afterStateUpdated(function (
+                        string $operation,
+                        string $state,
+                        Set $set,
+                        Get $get,
+                        Category $category
+                    ) { // NOTE: Params names are important!!
+                        //dump('hello');  // testing
+                        //dump($operation, $state, $set);  // testing
+                        if ($operation === 'edit') return;
+                        $set('slug', Str::slug($state));
+                        //dump($get);
+                        //dump($category);
+                    }),
+
+                TextInput::make('slug')->required()
+                    ->minLength(1)->maxLength(100)
+                    ->unique(ignoreRecord: true)->maxLength(100),
             ]);
     }
 
